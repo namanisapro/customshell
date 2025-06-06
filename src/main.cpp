55f81e4aa6969d17eb411bfc39285000f3c2d007
Naman Rustagi <naman7799k@gmail.com>
2025-06-07 00:51:30 +0530

    Trigger test run

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+  while (true) {
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
